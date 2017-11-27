e5c24b62127216581e023777fcf12f4c51f52577
U-CT20170602\CTWLPC <zhw.geek@gmail.com>
2017-11-27 10:38:59 +0800

    Stdout宏定义修复

--- src/log/Stdout.h
+++ src/log/Stdout.h
@@ -7,7 +7,7 @@
 #define WHITE FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE
 #define YELLOW FOREGROUND_RED | FOREGROUND_GREEN
 #define MAGENTA FOREGROUND_RED | FOREGROUND_BLUE
-const WORD colors[5] = { CYAN, CYAN, WHITE | BOLD, YELLOW | BOLD, RED | BOLD , MAGENTA | BOLD };
+const WORD colors[6] = { CYAN, CYAN, WHITE | BOLD, YELLOW | BOLD, RED | BOLD , MAGENTA | BOLD };
 
 #define ETOOL_LOG_STDOUT(msg) \
 fwrite(msg, sizeof(char), strlen(msg), stdout)
@@ -25,11 +25,16 @@ SetConsoleTextAttribute(outHandle, origBufferInfo.wAttributes)
 #define RESET "\033[00m"
 #define BOLD "\033[1m"
 #define RED "\033[31m"
+#define RED_BOLD "\033[31m\033[1m"
 #define CYAN "\033[36m"
+#define CYAN_BOLD "\033[36m\033[1m"
 #define WHITE "\033[37m"
+#define WHITE_BOLD "\033[37m\033[1m"
 #define YELLOW "\033[33m"
+#define YELLOW_BOLD "\033[33m\033[1m"
 #define MAGENTA "\033[35m"
-const char *colors[5] = { CYAN, CYAN, WHITE ## BOLD, YELLOW ## BOLD, RED ## BOLD,  MAGENTA ## BOLD };
+#define MAGENTA_BOLD "\033[35m\033[1m"
+const char *colors[6] = { CYAN, CYAN, WHITE_BOLD, YELLOW_BOLD, RED_BOLD,  MAGENTA_BOLD };
 
 #define ETOOL_LOG_STDOUT(msg) \
 fwrite(msg, sizeof(char), strlen(msg), stdout)
@@ -42,7 +47,7 @@ fwrite(RESET, sizeof(char), strlen(RESET), stdout)
 
 #if defined(_android)
 #include <android/log.h>
-const android_LogPriority colors[5] = { ANDROID_LOG_VERBOSE, ANDROID_LOG_DEBUG, ANDROID_LOG_INFO, ANDROID_LOG_WARN, ANDROID_LOG_ERROR, ANDROID_LOG_FATAL };
+const android_LogPriority colors[6] = { ANDROID_LOG_VERBOSE, ANDROID_LOG_DEBUG, ANDROID_LOG_INFO, ANDROID_LOG_WARN, ANDROID_LOG_ERROR, ANDROID_LOG_FATAL };
 
 #define ETOOL_LOG_STDOUT(msg) \
 __android_log_write(colors[level], "etool", msg)
